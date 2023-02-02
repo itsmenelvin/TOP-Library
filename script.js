eb9e484acd7613871d633404f9c939b4bd6f9930
itsmenelvin <121491819+itsmenelvin@users.noreply.github.com>
2023-02-03 05:35:41 +0800

    Refactor object constructor into a class and functions

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const library = [{ name: 'The batman', page: 50, read: true }];
-
-function Book(name, author, page, read) {
-  this.name = name;
-  this.author = author;
-  this.pages = parseInt(page);
-  this.read = read;
-}
-
-const formContainer = document.querySelector('.form-container');
-const form = document.querySelector('.form');
-const formButton = document.querySelector('.add');
+const library = [];
 const main = document.querySelector('main');
 
-document.addEventListener('DOMContentLoaded', function () {
-  const showButton = document.querySelector('.show-button');
+class Book {
+	static uniqueId = 0;
 
-  showButton.addEventListener('click', showForm);
+	constructor(name, author, page, read) {
+		this.name = name;
+		this.author = author;
+		this.pages = Number(page);
+		this.read = read ? 'Yes' : 'No';
+		this.uniqueId = Book.uniqueId++;
+		library.push(this);
+		this.render();
+	}
 
-  formButton.addEventListener('click', function () {
-    let name = document.querySelector('#book_name').value;
-    let page = document.querySelector('#book_page').value;
-    let author = document.querySelector('#book_author').value;
-    let read = document.querySelector('#done_reading');
-    // this will hide the form
-    formContainer.classList.toggle('show');
-    form.classList.toggle('show');
-    if (name === '' || page === '' || author === '') return; //if empty it will return/hide without adding the empty value
-    // create an new book object
-    const newBook = new Book(name, author, page, !!read.checked); // !! convert the value into boolean
-    library.push(newBook); // push the newly created object into library array
-    // reset input value
-    resetInput();
-    //create a card
-    const div = document.createElement('div');
-    div.classList.add('book');
-    div.innerHTML = `
-        <div class="book-image">
-          <div class="image"></div>
-        </div>
-        <div class="book-name">${
-          newBook.name.charAt(0).toUpperCase() + newBook.name.slice(1) //auto capitalize the first letter of the book name
-        }</div>
-        <div class="book-author">by: ${newBook.author}</div>
-        <div class="book-pages">PAGES: ${newBook.pages}</div>
-        <div class="book-read">READ: ${newBook.read ? 'Done' : 'Not yet'}</div>
-        <div class="book-edit">EDIT</div>
-        <div class="book-remove">REMOVE</div>
-    `;
-    main.appendChild(div);
-  });
+	render() {
+		const div = document.createElement('div');
+		div.setAttribute('data-key', this.uniqueId);
+		div.innerHTML = `
+        <div class="book-name">${this.name}</div>
+        <div class="book-author">${this.author}</div>
+        <div class="book-page">${this.pages}</div>
+        <div class="book-read">${this.read}</div>
+        <button class="edit-button">Edit</button>
+        <button class="remove-button">Remove</button>
+      `;
+		main.append(div);
+		this.listenRemove();
+	}
+
+	listenRemove() {
+		const removeButton = document.querySelectorAll('.remove-button');
+		removeButton.forEach(removeBtn => {
+			removeBtn.addEventListener('click', e => {
+				const index = Number(e.target.parentElement.getAttribute('data-key'));
+				const book = library.find(b => b.uniqueId === index);
+				library.splice(library.indexOf(book), 1);
+				e.target.parentElement.remove();
+			});
+		});
+	}
+}
+
+// sample book
+new Book('To Kill a Mockingbird', 'Harper Lee', 274, true);
+new Book('1984', 'George Orwell', 328, false);
+new Book('Pride and Prejudice', 'Jane Austen', 278, true);
+
+function addBook() {
+	const [name, author, pages] = [
+		...document.querySelectorAll("input:not([type='checkbox'])"),
+	];
+	const read = document.querySelector('[type="checkbox"]');
+	new Book(name.value, author.value, pages.value, !!read.checked);
+}
+
+const form = document.querySelector('.form');
+const headerButton = document.querySelector('.show-button');
+const formButton = document.querySelector('.add');
+
+headerButton.addEventListener('click', () => form.classList.add('show'));
+formButton.addEventListener('click', () => {
+	addBook();
+	form.classList.remove('show');
 });
-
-function resetInput() {
-  document.querySelector('#book_name').value = '';
-  document.querySelector('#book_page').value = '';
-  document.querySelector('#done_reading').checked = false;
-}
-
-function showForm() {
-  //this will show the form
-  formContainer.classList.add('show');
-  form.classList.add('show');
-}
